711fcd46d9c68a33af2f98e6610d27881d054df9
SGumerov <shamusg12345@gmail.com>
2020-07-10 14:17:33 +1100

    Домашнее задание к уроку 1

---
+++ 1.2.py
@@ -0,0 +1,7 @@
+# 2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
+# Используйте форматирование строк.
+user_input = int(input('Введите время в секундах: '))
+hours = user_input // 3600
+minutes = (user_input % 3600) // 60
+seconds = user_input % 60
+print('{:02}:'.format(hours), '{:02}:'.format(minutes), '{:02}'.format(seconds), sep="")
